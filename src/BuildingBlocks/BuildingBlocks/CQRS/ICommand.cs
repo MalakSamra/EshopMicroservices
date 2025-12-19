efc2f153f3adccb6ce798ab0421be1338a83fa8c
MalakEl_Samra <malaksamra1997@gmail.com>
2025-12-19 16:59:05 +0400

    jkjk

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+//Empty ICommand (Don't Return Any Response)
+public interface ICommand : ICommand<Unit>
+{
+}
+
+//Command That Produce a Response
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    //Command Object
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category, 
+        string Description,
+        string ImageFile,
+        decimal Price):IRequest<CreateProductResult>;
+
+    //Result Object
+    public record CreateProductResult(
+        Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business Logic to create a product
+            throw new NotImplementedException();
+        }
+    }
+}
